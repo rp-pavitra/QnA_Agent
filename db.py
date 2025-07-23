1a17c8ecb3c300cdb92cfb75f620c89086d5c218
R P Pavitra <106442966+rp-pavitra@users.noreply.github.com>
2025-07-23 20:46:50 +0530

    Update db.py, replacing sqlite3 library with duckdb to support large database

--- db.py
+++ db.py
@@ -1,52 +1,51 @@
-import sqlite3  
-import pandas as pd  
-  
+import duckdb
+import pandas as pd
 
 def get_connection():
     """
-    Function to create a SQLite database connection.
-    
+    Function to create a DuckDB database connection.
+
     Returns:
-    conn (Connection): SQLite database connection object.
+    conn (duckdb.DuckDBPyConnection): DuckDB database connection object.
     """
-    conn = sqlite3.connect('data.db')  
+    conn = duckdb.connect('data.db')
     cursor = conn.cursor()
-    return conn,cursor
+    return conn, cursor
 
-
-def store_file_data_in_db(df):
+def store_file_data_in_db(df, column_names):
     """
-    Function to store DataFrame in SQLite database.
-    
+    Function to store DataFrame in DuckDB database.
     Parameters:
     df (DataFrame): The DataFrame to be stored in the database.
     """
-    
-    # Get SQLite database connection  
-    conn,cursor = get_connection()
-
-    # Convert the DataFrame to an SQL table  
-    df.to_sql('file_data', conn, if_exists='replace', index=False)  
-    
-    # Close the connection  
-    conn.close()  
-    status = True
+    print("Storing file data in DuckDB database...")
+    # Get DuckDB database connection
+    conn, _ = get_connection()
+    try:
+        # Write DataFrame to DuckDB table
+        conn.execute("DROP TABLE IF EXISTS file_data")
+        columns_str = ', '.join([f'"{col}"' for col in column_names])
+        conn.register('df_view', df)
+        conn.execute(f"CREATE TABLE file_data AS SELECT {columns_str} FROM df_view")
+        conn.unregister('df_view')
+        conn.close()
+        status = True
+        print("File data stored in DuckDB database successfully.")
+    except Exception as e:
+        print(f"Error storing file data in DuckDB database: {str(e)}")
+        status = False
+        # conn.close()    
     return status
-
 
 def get_schema():
     """
-    Function to get the schema of the SQLite database table.
+    Function to get the schema of the DuckDB database table.
     """
-    # Get SQLite database connection  
-    conn,cursor = get_connection() 
-    
-    # Get the schema of the table  
-    cursor.execute("PRAGMA table_info(file_data)")  
-    schema = cursor.fetchall()  
-    
-    # Close the connection  
-    conn.close()  
-    
+    conn, cursor = get_connection()
+    print("Retrieving schema of the DuckDB table...")
+    # Get the schema of the table
+    cursor.execute("PRAGMA table_info('file_data')")
+    schema = cursor.fetchall()
+    conn.close()
+    print("Schema retrieved successfully.")
     return schema
-  
